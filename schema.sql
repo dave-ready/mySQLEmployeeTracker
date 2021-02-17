afe2941af9bb3ac26387d4e5e76b24eed19d1f87
dave ready <davis_ready@hotmail.com>
2021-02-17 08:39:53 -0500

    schema edit

--- schema.sql
+++ schema.sql
@@ -4,29 +4,25 @@ CREATE database employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER(10) AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role_info (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL,
-  INDEX department_index (department_id),
-  CONSTRAINT department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  salary DECIMAL NOT NULL,
+  department_id INTEGER(10) NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER(10) AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
-  INDEX role_index (role_id),
-  CONSTRAINT role_info FOREIGN KEY (role_id) REFERENCES role_info(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
-  INDEX manager_index (manager_id),
-  CONSTRAINT manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  role_id INTEGER(10) NOT NULL,
+  manager_id INTEGER(10),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 
